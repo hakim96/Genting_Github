69a66756fe33d498631c89b663e54eb882805bcb
Ruion <liewyeat@gmail.com>
2019-10-29 20:13:09 +0800

    Page animation and pageflipper component added to buttons

---
+++ 191005-MY-Genting/Assets/Script/UI/PageFlipper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageFlipper : MonoBehaviour
+{
+    public Animator prevAnimator;
+    public GameObject nextPage;
+
+    public void FlipNextPage()
+    {
+        prevAnimator.Play("pageCloseClip");
+        nextPage.SetActive(true);
+    }
+}
